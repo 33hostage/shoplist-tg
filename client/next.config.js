71197946dc6632bee1e61e12a527282592c216ba
33hostage <142483710+33hostage@users.noreply.github.com>
2025-10-20 14:51:59 +0300

    fix: Temporarily romove next-pwa to resolve Vercel deploy

--- client/next.config.js
+++ client/next.config.js
@@ -1,24 +1,18 @@
-const withPWA = require("next-pwa")({
-	dest: "public",
-	disable: process.env.NODE_ENV === "development",
-	register: true,
-	skipWaiting: true,
-})
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	// Прокси для dev-режима
-	async rewrites() {
-		if (process.env.NODE_ENV === "development") {
-			return [
-				{
-					source: "/api/:path*",
-					destination: "http://localhost:4000/api/:path*",
-				},
-			]
-		}
-		return []
-	},
-}
 
-module.exports = withPWA(nextConfig)
+  // Прокси для dev-режима (остается)
+  async rewrites() {
+    if (process.env.NODE_ENV === "development") {
+      return [
+        {
+          source: "/api/:path*",
+          destination: "http://localhost:4000/api/:path*",
+        },
+      ];
+    }
+    return [];
+  },
+};
+
+module.exports = nextConfig;
